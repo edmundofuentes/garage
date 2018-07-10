c888d34cebc3f88ac70a293802b8e60c29dceb99
Edmundo Fuentes <me@edmundofuentes.com>
2018-07-09 23:40:31 -0500

    Forking my main blog, creating a garage!

--- publish.sh
+++ publish.sh
@@ -5,5 +5,5 @@ vendor/bin/sculpin generate --env=prod
 if [ $? -ne 0 ]; then echo "Could not generate the site"; exit 1; fi
 
 echo "## Netlify Deploy"
-# push it to netlify
-netlify deploy
+# push it to netlify using the newer netlifyctl tool
+netlifyctl deploy -P output_prod
---
+++ source/_posts/2018-07-01-installing-new-fork.md
@@ -0,0 +1,10 @@
+---
+title: Installing a New Fork
+tags:
+    - road
+    - enduro
+categories:
+    - bicycles
+---
+
+Woah, new tests, more tests.
