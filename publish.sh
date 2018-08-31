a419ff5e6a6b54dc0c90265bcca209ebb3ff39ae
Edmundo Fuentes <mundofr@gmail.com>
2018-08-31 17:45:58 -0500

    Adding author metadata, updating publish method

--- publish.sh
+++ publish.sh
@@ -4,6 +4,12 @@ echo "## Sculpin Generate"
 vendor/bin/sculpin generate --env=prod
 if [ $? -ne 0 ]; then echo "Could not generate the site"; exit 1; fi
 
+## Rename .html.twig files to .html only
+echo "FIX: Cleaning up the filenames"
+cd output_prod
+find . -name '*.html.twig' -exec sh -c 'mv "$0" "${0%.html.twig}.html"' {} \;
+cd ..
+
 echo "## Netlify Deploy"
 # push it to netlify using the newer netlifyctl tool
 netlifyctl deploy -P output_prod
